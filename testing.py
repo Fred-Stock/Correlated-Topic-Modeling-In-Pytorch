3f9f52a30aa3886b11fab52c152aebe92d391d02
anugrahvaishnav <anugrah.vaishnav@portcast.io>
2023-10-31 17:14:40 -0400

    Revert "Just getting hands-on with Git"
    This reverts commit e86ba728a1abd334cc92d6a9970b3b84a987c60d.

--- testing.py
+++
@@ -1,9 +0,0 @@
-# write a function to print 'Hello World!'
-def custom_print_function(n):
-    string = 'Hello World!'
-    for i in range(n):
-        print(string)
-    
-if __name__ == '__main__':
-    n = int(input('How many times do you want to print Hello World?'))
-    custom_print_function(n)
